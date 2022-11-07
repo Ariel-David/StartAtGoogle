89d3a851fd79b3083f33c29cde92e40b5e1961f5
Ariel <arieldavid642@gmail.com>
2022-11-07 14:43:23 +0200

    change some files

--- ClassExercise/src/AuthenticationServiceProject/main/java/Controllers/ControllersUtil.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
- class ControllersUtil {
-    public static void printErrorToCmd(String mainMessage,String defiles)
-    {
-        System.out.println(mainMessage);
-        System.out.println(defiles);
-    }
-}
